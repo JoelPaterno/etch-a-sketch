7a95b878b8046f5fe799eba13c22affb83e2aa40
joelpaterno <joelpaterno1@gmail.com>
2024-12-24 11:12:28 +1100

    feat: add change canvas size option

--- index.js
+++ index.js
@@ -1,28 +1,42 @@
 const container = document.querySelector(".container");
 let resolution = 16;
-let count = 1;
-
 
 function createGrid(res) {
-    let flexBasis = Math.floor(100/res);
-    //create a grid of divs
+    let flexBasis = (100/res);
     for (let i = 0; i < res; i++) {
         for (let i = 0; i < res; i++) {
             let pixel = document.createElement("div");
             pixel.classList.add("pixel");
             pixel.style.cssText = `flex-basis: ${flexBasis}%;`;
             container.appendChild(pixel);
-            count++;
         } 
     }
 }
 
 createGrid(resolution);
 
-let pixels = document.querySelectorAll(".pixel");
 
-pixels.forEach((pixel) => {
-    pixel.addEventListener("mouseover", () => {
-        pixel.classList.add('pixel-color');
+function addPixelEventListenrs() {
+    let pixels = document.querySelectorAll(".pixel");
+
+    pixels.forEach((pixel) => {
+        pixel.addEventListener("mouseover", () => {
+            pixel.classList.add('pixel-color');
+        });
     });
+};
+const resButton = document.querySelector(".resolution");
+
+resButton.addEventListener("click", () => {
+    let chosenRes = 101;
+    while (chosenRes > 100) {
+        chosenRes = prompt("What resolution would you like (max 100)?");
+    };
+    let pixels = document.querySelectorAll(".pixel");
+    pixels.forEach((pixel) => {
+        container.removeChild(pixel);
+    });
+    createGrid(chosenRes);  
+    addPixelEventListenrs();
 });
+     
